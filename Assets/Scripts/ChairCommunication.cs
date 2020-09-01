1c36c8ebf01862e6ba635d72aee3d5225389bc2a
M Bethancourt <matt@mouseandthebillionaire.com>
2020-09-01 12:52:14 -0600

    File reorg and setup for final implementation. Let's do this!

---
+++ Assets/Scripts/ChairCommunication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairCommunication: MonoBehaviour
+{
+    void OnMessageArrived(string msg) {
+        string[] incomingValues = msg.Split(' ');
+        float pitch = float.Parse(incomingValues[0]);
+
+        if (pitch > -30f) {
+            //TreeFade.S.StartCoroutine("AdvanceAnimation");
+            Debug.Log("Doing it");
+        }
+    }
+    
+}
