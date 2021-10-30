0725b5ff520a6a85cc0c91ad72d507ff3c1d55e0
M Bethancourt <matt@mouseandthebillionaire.com>
2021-10-30 13:21:27 -0600

    Script location fix

---
+++ Assets/Scripts/AudioControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioControl : MonoBehaviour
+{
+    	
+    // Get Init Strum
+    public AudioSource[]		strum, backStrum;
+    public AudioMixerSnapshot[] audioStates;
+
+    public static AudioControl  S;
+    
+    void Awake() {
+        S = this;
+        audioStates[0].TransitionTo(0.1f);
+    }
+
+    void Start() {
+        Reset();
+    }
+
+    public void ChairControlledSound(int chairNum, int chairDirection) {
+        if (GameManager.S.audioStage >= 0) {
+            if (chairDirection == 1) {
+                // back
+                // play the preliminary guitar note
+                strum[chairNum].Play();
+            }
+        }
+        
+    }
+
+    public void NextStage(float speed) {
+        GameManager.S.audioStage++;
+        audioStates[GameManager.S.audioStage].TransitionTo(speed);
+        Debug.Log("Loading Stage: " + GameManager.S.audioStage);
+    }
+    
+    public void LoadStage(int stage, float speed) {
+        GameManager.S.audioStage = stage;
+        audioStates[stage].TransitionTo(speed);
+        Debug.Log("Loading Stage: " + GameManager.S.audioStage);
+    }
+
+    public void Reset() {
+        audioStates[0].TransitionTo(2f);
+        GameManager.S.audioStage = 0;
+
+
+    }
+}
---
+++ Assets/Scripts/BuildingAnimations.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingAnimations : MonoBehaviour {
+    public  GameObject             animationCell;
+    public float                   animationDelay;
+    private int                    cellNum;
+    private Sprite[]               topo, landFormation, treeSprites;
+    private List<Sprite[]>         animations = new List<Sprite[]>();        
+    private string[]               animationNames = {"topo", "landFormation"};
+    private int                    currAnimation;
+    private bool                   animationComplete;
+    
+    
+    // timing
+    private float m;
+    private float prevM;
+
+    public static BuildingAnimations S;
+
+    void Awake() {
+        S = this;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        topo = Resources.LoadAll<Sprite>("Topo"); 
+        landFormation = Resources.LoadAll<Sprite>("landFormation");
+        treeSprites = Resources.LoadAll<Sprite>("TreeSprites");
+        animations.Add(landFormation);
+        animations.Add(topo);
+        animations.Add(treeSprites);
+        Reset();
+    }
+
+    void FixedUpdate() {
+        // timer
+        m = Time.time - prevM;
+        if (m >= animationDelay) {
+            if (GameManager.S.gameStage > 0) {
+                AddAnimationCell();
+            }
+            prevM = Time.time;
+        }
+        
+        // Testing
+    }
+
+    public void AddAnimationCell() {
+        if (cellNum < animations[currAnimation].Length && !animationComplete) {
+            GameObject     go = GameObject.Instantiate(animationCell) as GameObject;
+            SpriteRenderer sr = go.GetComponent<SpriteRenderer>();
+            sr.sprite = animations[currAnimation][cellNum];
+            go.transform.parent = this.transform;
+            go.name = cellNum.ToString();
+            cellNum++;
+        }
+        if (cellNum >= animations[currAnimation].Length) {
+            animationComplete = true;
+            Debug.Log("I'm still true?");
+            StartCoroutine("HoldAndClear");
+        }
+
+    }
+
+    public IEnumerator HoldAndClear() {
+        yield return new WaitForSeconds(20);
+        ClearAnimations();
+    }
+
+
+    public void ClearAnimations() {
+        // delete all children
+        int childs = this.transform.childCount;
+        for (int i = 0; i < childs; i++)
+        {
+            GameObject.Destroy(transform.GetChild(i).gameObject);
+        }
+    }
+
+    public void PrepStage(int STL, float AD) {
+        cellNum = 0;
+        currAnimation = STL;
+        prevM = Time.time;
+        animationDelay = AD;
+        animationComplete = false;
+        ClearAnimations();
+    }
+
+    public void Reset() {
+        cellNum = 0;
+        m = 0;
+        prevM = 0;
+        animationDelay = 0.25f;
+        currAnimation = 0;
+        animationComplete = false;
+        ClearAnimations();
+    }
+}
